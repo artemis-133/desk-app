44626123af0bfd2b67457094ae9988a8982e9287
artemis-133 <sidcoding133@gmail.com>
2023-10-05 16:38:12 +0530

    Historytable added

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import CommonSection from "../components/Commonsection";
+import Helmet from "../components/Helmet";
+import "../styles/history.css";
 
 const History = () => {
+  const [bookingHistory, setBookingHistory] = useState([
+    {
+      employeeName: "Siddharth Mishra",
+      dateOfBooking: "2023-10-01",
+      deskNumber: "Desk A1",
+    },
+    {
+      employeeName: "Manik Garg",
+      dateOfBooking: "2023-09-25",
+      deskNumber: "Desk B3",
+    },
+    {
+      employeeName: "Amit Sehgal",
+      dateOfBooking: "2023-10-02",
+      deskNumber: "Desk B13",
+    },
+    // Add more booking history entries here
+  ]);
   return (
-    <div>History</div>
-  )
-}
+    <Helmet title="History">
+      <CommonSection title="Booking History" />
+      <section>
+        <div className="history-container">
+          <h1>Booking History</h1>
+          <table className="history-table">
+            <thead>
+              <tr>
+                <th>Employee Name</th>
+                <th>Date of Booking</th>
+                <th>Desk Number</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bookingHistory.map((booking, index) => (
+                <tr key={index}>
+                  <td>{booking.employeeName}</td>
+                  <td>{booking.dateOfBooking}</td>
+                  <td>{booking.deskNumber}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </Helmet>
+  );
+};
 
-export default History
+export default History;
