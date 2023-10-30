a717e34e2ba24a892510fa00f2f852fef2c84b7d
artemis-133 <sidcoding133@gmail.com>
2023-10-30 14:15:45 +0530

    Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homepage">
+      <div className="content">
+        <h1>Welcome to Desk Booking</h1>
+        <p>Your destination for hassle-free desk reservations.</p>
+        <button className="get-started-button">Let's Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
